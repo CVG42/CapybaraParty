14b4b244cefd9f41e9241318484a3be94307c733
CVG42 <zap653@amerike.edu.mx>
2022-11-22 14:09:07 -0600

    Health Bar and distance debug

--- Assets/Scripts/Enemy/AIShooting.cs
+++ Assets/Scripts/Enemy/AIShooting.cs
@@ -19,7 +19,6 @@ public class AIShooting : MonoBehaviour
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
 
         if (distance < maxDistance)
         {
